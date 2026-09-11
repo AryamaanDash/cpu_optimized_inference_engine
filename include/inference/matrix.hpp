c4f202298c46ea1d87feefba917cd9f0917111f7
Aryamaan Dash <113083416+Crumpley7@users.noreply.github.com>
2026-09-11 01:42:10 -0700

    set up matrix header file

---
+++ include/inference/matrix.hpp
@@ -0,0 +1,43 @@
+#ifndef MATRIX_HPP
+#define MATRIX_HPP
+#include <cstddef>
+#include <vector>
+
+namespace inference {
+
+class Matrix {
+public:
+    Matrix(std::size_t rows, std::size_t cols);
+
+// getters for rows and cols
+    std::size_t rows() const noexcept;
+    std::size_t cols() const noexcept;
+
+// functions to view (const) a specific value in the matrix
+    float& operator()(std::size_t row, std::size_t col);
+    const float& operator()(std::size_t row, std::size_t col) const;
+
+    float* data() noexcept;
+    const float* data() const noexcept;
+
+private:
+    std::size_t rows_;
+    std::size_t cols_;
+    std::vector<float> data_;
+};
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif // MATRIX_HPP
