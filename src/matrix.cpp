86856fae114ec58b74e73ff722fdb5ea162dfe21
Aryamaan Dash <113083416+Crumpley7@users.noreply.github.com>
2026-09-11 02:14:01 -0700

    defined entire matrix class and copy + move semantics

---
+++ src/matrix.cpp
@@ -0,0 +1,63 @@
+#include "inference/matrix.hpp"
+#include <stdexcept>
+
+namespace inference{
+    Matrix::Matrix(std::size_t rows, std::size_t cols){
+        if(cols != 0 && rows > data_.max_size() / cols){
+            throw std::length_error("Matrix dimensions are too large");
+        }
+        rows_ = rows;
+        cols_ = cols;
+        data_.resize(rows * cols, 0.0f);
+    }
+
+    Matrix::Matrix(Matrix&& other) noexcept
+    : rows_(other.rows_), cols_(other.cols_), data_(std::move(other.data_))
+    {
+        other.rows_ = 0;
+        other.cols_ = 0;
+        other.data_.clear();
+    }
+
+    Matrix& Matrix::operator=(Matrix&& other) noexcept{
+        if(this != &other){
+        rows_ = other.rows_;
+        cols_ = other.cols_;
+        data_ = std::move(other.data_);
+        other.rows_ = 0;
+        other.cols_ = 0;
+        other.data_.clear();
+        }
+        return *this;
+    }
+
+    std::size_t Matrix::rows() const noexcept{
+        return rows_;
+    }
+
+    std::size_t Matrix::cols() const noexcept{
+        return cols_;
+    }
+
+    float& Matrix::operator()(std::size_t row, std::size_t col){
+        if(row >= rows_ || col >= cols_){
+            throw std::out_of_range("Given input is out of range");
+        }
+        return data_[row * cols_ + col];
+    }
+
+    const float& Matrix::operator()(std::size_t row, std::size_t col) const{
+        if(row >= rows_ || col >= cols_){
+            throw std::out_of_range("Given input is out of range");
+        }
+        return data_[row * cols_ + col];
+    }
+
+    float* Matrix::data() noexcept{
+        return data_.data();
+    }
+    
+    const float* Matrix::data() const noexcept{
+        return data_.data();
+    }
+}
