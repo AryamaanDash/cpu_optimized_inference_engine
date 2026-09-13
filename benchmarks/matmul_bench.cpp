ffdefc59881b36dea9e17a6699e1bd0b53bce53a
Aryamaan Dash <113083416+Crumpley7@users.noreply.github.com>
2026-09-12 17:51:12 -0700

    set up google benchmark to run with matmul reference

--- benchmarks/matmul_bench.cpp
+++ benchmarks/matmul_bench.cpp
@@ -1,18 +1,24 @@
 #include "inference/matrix.hpp"
 
+
 #include <benchmark/benchmark.h>
 #include <cstddef>
 
 static void BM_MatmulReferenceAllocationIncluded(benchmark::State& state){
-  const auto size = static_cast<std::size_t>(state.range(0));
 
-  inference::Matrix a(size, size);
-  inference::Matrix b(size, size);
+  const auto m = static_cast<std::size_t>(state.range(0));
+  const auto k = static_cast<std::size_t>(state.range(1));
+  const auto n = static_cast<std::size_t>(state.range(2));
 
-  for (std::size_t i = 0; i < size * size; ++i){
+  inference::Matrix a(m, k);
+  inference::Matrix b(k, n);
+
+  for(std::size_t i = 0; i < m * k; ++i){
     a.data()[i] = static_cast<float>(static_cast<int>(i % 17) - 8) / 8.0f;
-    b.data()[i] = static_cast<float>(static_cast<int>(i % 13) - 6) / 6.0f;
   }
+
+  for(std::size_t i = 0; i < k * n; ++i)
+    b.data()[i] = static_cast<float>(static_cast<int>(i % 13) - 6) / 6.0f;
 
   for(auto _ : state){
     auto result = inference::matmul_reference(a, b);
@@ -24,5 +30,12 @@ static void BM_MatmulReferenceAllocationIncluded(benchmark::State& state){
 }
 
 BENCHMARK(BM_MatmulReferenceAllocationIncluded)
-  ->Arg(32)
+  ->ArgNames({"M","K","N"})
+  ->Args({1, 1, 1})
+  ->Args({32, 32, 32})
+  ->Args({128, 128, 128})
+  ->Args({256, 256, 256})
+  ->Args({63,65,67})
+  ->Args({1, 256, 256})
+  ->Args({256,256,1})
   ->Unit(benchmark::kMicrosecond);
